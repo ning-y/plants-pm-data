300b49432d9b90b7dda1d74ace08f166d401ca84
ning <ningyuan.sg@gmail.com>
2019-10-16 09:40:59 +0800

    Add 15oct data for sensor calibration

--- data/sensor-calibration/plot.R
+++ data/sensor-calibration/plot.R
@@ -48,19 +48,26 @@ trial3 <- get_trial('14octb',
     '14octb_nothing_2bigbox.csv',
     '14octb_nothing_3bigbox.csv',
     '14octb_nothing_4bigbox.csv')
+trial4 <- get_trial('15octa',
+    '15octa-nothing-1bigbox.csv',
+    '15octa-nothing-2bigbox.csv',
+    '15octa-nothing-3bigbox.csv',
+    '15octa-nothing-4bigbox.csv')
 
 trial1.pm25 <- subset(trial1, type=='pm2.5')
 trial2.pm25 <- subset(trial2, type=='pm2.5')
 trial3.pm25 <- subset(trial3, type=='pm2.5')
+trial4.pm25 <- subset(trial4, type=='pm2.5')
 trial1.pm25$time <- trial1.pm25$time - time_of_first_desaturation(trial1.pm25, 999.9)
 trial2.pm25$time <- trial2.pm25$time - time_of_first_desaturation(trial2.pm25, 999.9)
 trial3.pm25$time <- trial3.pm25$time - time_of_first_desaturation(trial3.pm25, 999.9)
-trials.pm25 <- rbind(trial1.pm25, trial2.pm25, trial3.pm25)
+trial4.pm25$time <- trial4.pm25$time - time_of_first_desaturation(trial4.pm25, 999.9)
+trials.pm25 <- rbind(trial1.pm25, trial2.pm25, trial3.pm25, trial4.pm25)
 trials.pm25 <- subset(trials.pm25, time >= 0 & time <= 20000)
 
 ggplot(trials.pm25) +
     geom_line(mapping=aes(x=time, y=value, colour=sensor, linetype=trial)) +
-    scale_linetype_manual(values=c('solid', 'longdash', 'dotted')) +
+    scale_linetype_manual(values=c('dashed', 'dotdash', 'solid', 'dotted')) +
     labs('PM2.5 for sensors in the same box') +
     xlab('Time since first report of valid reading per trial (s)')
 
@@ -69,15 +76,17 @@ ggsave('pm25.png', width=16, height=9)
 trial1.pm10 <- subset(trial1, type=='pm10')
 trial2.pm10 <- subset(trial2, type=='pm10')
 trial3.pm10 <- subset(trial3, type=='pm10')
+trial4.pm10 <- subset(trial4, type=='pm10')
 trial1.pm10$time <- trial1.pm10$time - time_of_first_desaturation(trial1.pm10, 1986.7)
 trial2.pm10$time <- trial2.pm10$time - time_of_first_desaturation(trial2.pm10, 1986.7)
 trial3.pm10$time <- trial3.pm10$time - time_of_first_desaturation(trial3.pm10, 1986.7)
-trials.pm10 <- rbind(trial1.pm10, trial2.pm10, trial3.pm10)
+trial4.pm10$time <- trial4.pm10$time - time_of_first_desaturation(trial4.pm10, 1986.7)
+trials.pm10 <- rbind(trial1.pm10, trial2.pm10, trial3.pm10, trial4.pm10)
 trials.pm10 <- subset(trials.pm10, time >= 0 & time <= 30000)
 
 ggplot(trials.pm10) +
     geom_line(mapping=aes(x=time, y=value, colour=sensor, linetype=trial)) +
-    scale_linetype_manual(values=c('solid', 'longdash', 'dotted')) +
+    scale_linetype_manual(values=c('dashed', 'dotdash', 'solid', 'dotted')) +
     labs('PM10 for sensors in the same box') +
     xlab('Time since first report of approx valid reading (1986.7) per trial (s)')
 
